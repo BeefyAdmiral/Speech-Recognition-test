54b181569002834764abb4b17ad37608536d6ff1
Anshul Dhariwal <anshuldhariwal04@gmail.com>
2022-09-15 18:07:54 -0700

    Added parallel commuting for speech recognition

--- Initial-speech-reg.py
+++ Initial-speech-reg.py
@@ -1,41 +1,74 @@
+import logging
+import threading
+import time
+import concurrent.futures
 import speech_recognition as sr
 from playsound import playsound
+
+
+def thread_function(name):
+    # Using Google API to identify the text
+    with sr.Microphone() as source:
+    
+        # Adjust for ambient noise before recognition
+        r.adjust_for_ambient_noise(source)
+        print("Setup complete")
+        
+        while True:
+            print("running")
+            if len(found) != 0:
+                break
+            else:
+                audio_data.append(r.listen(source))
+                print("test")
+
+def thread_function2(name):
+    while True:
+        if len(audio_data) != 0:
+            t = audio_data.pop(0)
+            text = r.recognize_google(t)
+            text = text.split()
+            print(text)
+            if "hello" in text:
+                found.append(1)
+                print("found")
+                break
+            
+    playsound('notification.mp3')
+    is_command_given = False
+    
+    with sr.Microphone() as source:
+        # Wait for the command
+        while is_command_given == False:
+            print(is_command_given)
+            command = r.listen(source)
+            print("Recognizing...")
+        
+            # Standing by till user speaks a specific word. In this case - "Hello"
+            try:
+                text = r.recognize_google(command)
+                is_command_given = True
+                # After response is received print the response and close the
+                # program
+                print(text)
+                
+            # If user taking too long then stand-by for a response
+            except:
+                print("Failed!")
+                is_command_given = False
+            
 
 # Building the recognizer model
 r = sr.Recognizer()
 
-# Check if command was given
-is_command_given = False
+# Variables to store the audio data and specific text found
+audio_data = []
+found = []
 
-# Using Google API to identify the text
-with sr.Microphone() as source:
+# Creating 2 threads, one to listen to audio source and other to detect the 
+# initial response
+x = threading.Thread(target=thread_function, args=(1,))
+y = threading.Thread(target=thread_function2, args=(1,))
     
-    # Adjust for ambient noise before recognition
-    r.adjust_for_ambient_noise(source)
-    print("Setup complete")
-    
-    # read the audio data from the default microphone
-    while is_command_given == False:
-        audio_data = r.listen(source)
-        print("Recognizing...")
-        
-        # Standing by till user speaks a specific word. In this case - "Hello"
-        try:
-            text = r.recognize_google(audio_data)
-            text = text.split()
-            
-            # Confirming the word and alerting the user
-            if "hello" in text:
-                playsound('notification.mp3')
-                command = r.listen(source)
-                req = r.recognize_google(command)
-                
-                # After response is received print the response and close the
-                # program
-                print(req)
-                is_command_given = True
-                
-        # If user taking too long then stand-by for a response
-        except:
-            print("Failed!")
-            is_command_given = False
+x.start()
+y.start()
